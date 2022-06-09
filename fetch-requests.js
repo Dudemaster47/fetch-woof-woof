2cf8b8fd3978163eb3e24805cff2e95d5214f7d1
Andre <andre.christian.silva@gmail.com>
2022-06-08 20:24:55 -0700

    my job is over

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,40 +5,52 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-fetch('/products',{
-    method: 'POST',
-    body:"name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",   
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-        }
+fetch('/products', {
+  method: 'POST',
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
 
-    })  
+})
 
 /* ============================== Phase 2 ============================== */
 
-async function postObj(){
- 
-    const resObj = await fetch('/products',{
-        method: 'POST',
-        body:"name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",   
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-            }
-    
-        })  
-    const status = resObj.status
-    
-    const url = resObj.url
-    
-    const header = resObj.headers.get("Content-Type")
-    
-        console.log(status, url, header)
-    
+async function postObj() {
+
+  const resObj = await fetch('/products', {
+    method: 'POST',
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
     }
-    
-    postObj()
+
+  })
+  const status = resObj.status
+
+  const url = resObj.url
+
+  const header = resObj.headers.get("Content-Type")
+
+  console.log(status, url, header)
+
+}
+
+postObj()
 
 
 /* ============================== Phase 3 ============================== */
+const coffee = new URLSearchParams({
+  name: "Caribbean Delight Coffee",
+  description: "Made by Manatee Coffee",
+  price: 11.99,
+  categories: "grocery"
+});
+fetch('/products', {
+  method: 'POST',
+  body: coffee.toString(),
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
 
-// Your code here
+})
