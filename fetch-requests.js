7f891dfa02007ef1ba8adec68f4c62bfdfe8226c
jimmy <jimmyfam96@gmail.com>
2022-06-08 20:01:11 -0700

    phase2

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,14 +5,38 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
+fetch('/products',{
+    method: 'POST',
+    body:"name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",   
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+        }
 
-
-
+    })  
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here
-
+async function postObj(){
+ 
+    const resObj = await fetch('/products',{
+        method: 'POST',
+        body:"name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",   
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+            }
+    
+        })  
+    const status = resObj.status
+    
+    const url = resObj.url
+    
+    const header = resObj.headers.get("Content-Type")
+    
+        console.log(status, url, header)
+    
+    }
+    
+    postObj()
 
 
 /* ============================== Phase 3 ============================== */
